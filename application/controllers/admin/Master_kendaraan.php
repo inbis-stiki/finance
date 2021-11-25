bc43a73ab791ce564c2c136c269f67438383e5d6
TubagusEza <9g.tubaguseza@gmail.com>
2021-11-25 16:20:08 +0700

    master kendaraan tapi kurang edit

---
+++ application/controllers/admin/Master_kendaraan.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Master_kendaraan extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        if (empty($this->session->userdata('user_role'))) {
+            redirect('/');
+        }
+
+        $this->load->helper(array('form', 'url', 'date'));
+        $this->load->library('form_validation');
+        $this->load->model('M_kendaraan_master');
+    }
+
+    public function aksiTambahKendaraan()
+    {
+        //get foto
+        $config['upload_path'] = './assets/images/fotokendaraan';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size'] = '2048';  //2MB max
+        $config['max_width'] = '4480'; // pixel
+        $config['max_height'] = '4480'; // pixel
+        $config['file_name'] = $_FILES['foto']['name'];
+
+        $this->upload->initialize($config);
+
+        $data_lama = $this->db->get_where("master_kendaraan", array('kendaraan_no_rangka' => $this->input->post('rangka')))->row_array();
+
+        if (!empty($_FILES['foto']['name'])) {
+            if ($this->upload->do_upload('foto')) {
+                $fotokendaraan = $this->upload->data();
+                $data = array(
+                    'kendaraan_no_rangka'         => $this->input->post('rangka'),
+                    'kendaraan_stnk'             => $this->input->post('stnk'),
+                    'kendaraan_merk'             => $this->input->post('merk'),
+                    'kendaraan_tanggal_beli'     => $this->input->post('tanggal'),
+                    'kendaraan_foto'            => $fotokendaraan['file_name']
+                );
+
+                $this->db->query("CALL disable_kendaraan('".$data_lama['kendaraan_no_rangka']."')");
+                $query = $this->db->insert('master_kendaraan', $data);
+                if ($query = true) {
+                    $this->session->set_flashdata('inserted', 'Yess');
+                    redirect('admin/master_kendaraan');
+                }
+            } else {
+                die("gagal upload");
+            }
+        } else {
+        }
+    }
+
+    function editRegion()
+    {
+        $id = $this->input->post('region_id');
+        $kota = $this->input->post('region_kota');
+        $this->M_region->editRegion($kota, $id);
+        redirect('admin/Admin/master_region');
+    }
+}
--- application/views/_components/modal.php
+++ application/views/_components/modal.php
@@ -66,7 +66,49 @@
 </div>
 
 
-<!-- Modal Edit Region -->
+<!-- Modal Tambah Kendaraan -->
+<div class="modal fade" id="add_masterKendaraan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content p-2">
+            <div class="modal-header">
+                <p class="font-w-700 color-darker mb-0">Tambah Kendaraan</p>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <?= form_open_multipart('admin/Master_kendaraan/aksiTambahKendaraan'); ?>
+            <div class="modal-body row m-0 p-0 w-100">
+
+                <div class="col-12 col-lg-6 ps-0 d-flex flex-column">
+                    <label class="mb-3">Upload Foto Kendaraan</label>
+                    <div class="upload-img color-dark">
+                        <span class="iconify fs-80px mb-3 z-2" data-icon="ic:baseline-photo-camera"></span>
+                        <p class="z-2">Klik disini untuk upload foto</p>
+
+                        <input type="file" name="foto" accept="image/png, image/gif, image/jpeg" id="imageInput" class="z-2" required />
+                        <div class="z-2"></div>
+
+                        <img src="" class="image-preview">
+                    </div>
+                    <small><?php if (isset($error)) {
+                                echo $error;
+                            } ?></small>
+                </div>
+                <div class="col-12 col-lg-6 pe-0 d-flex flex-column justify-content-between">
+                    <label class="mb-3">Nomor STNK Kendaraan</label>
+                    <input type="text" class="login-input regular" name="stnk" placeholder="Masukkan nomor STNK kendaraan" required>
+                    <label class="my-3">Nomor Rangka Kendaraan</label>
+                    <input type="text" class="login-input regular" name="rangka" placeholder="Masukkan nomor Rangka kendaraan" required>
+                    <label class="my-3">Merk Kendaraan</label>
+                    <input type="text" class="login-input regular" name="merk" placeholder="Masukkan Merk Kendaraan" required>
+                    <label class="my-3">Tanggal Beli Kendaraan</label>
+                    <input type="date" class="login-input regular fs-16px" name="tanggal" id="datepicker" required>
+                </div>
+                <button type="submit" class="btn-table submit-modal">Tambah data</button>
+
+            </div>
+            <?= form_close() ?>
+        </div>
+    </div>
+</div>
 
 
 <!-- Modal Sukses -->
