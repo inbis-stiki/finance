146a69c83d517099fce487dc3cc0ef1254a13a0f
devisap <ilham.sagitaputra@gmail.com>
2022-04-25 08:19:20 +0700

    update 1

--- application/controllers/management/Laporan.php
+++ application/controllers/management/Laporan.php
@@ -36,4 +36,42 @@ class Laporan extends CI_Controller{
 		$this->template->index('admin/laporan_tahunan', $data);
 		$this->load->view('_components/sideNavigation', $data);
     }
+	public function getDataAdm($param){
+        $filter = "";
+        if($param['filter'] == '1'){ // getByMonth
+            $filter = '
+                YEAR(t.transaksi_tanggal) = "'.$param['year'].'"
+                AND MONTH(t.transaksi_tanggal) = "'.$param['month'].'"
+            ';
+        }else if($param['filter'] == "2"){
+            $filter = '
+                t.transaksi_tanggal >= "'.$param['start'].'"
+                AND t.transaksi_tanggal <= "'.$param['end'].'"
+            ';
+        }else if($param['filter'] == "3"){
+            $filter = '
+                t.transaksi_tanggal = "'.$param['date'].'"
+            ';
+        }
+
+        return $this->db->query('
+            SELECT
+                t.*,
+                mjp.pengeluaran_group as jenis_pengeluaran,
+                mjp.pengeluaran_jenis as pengeluaran,
+                SUM(t.transaksi_jumlah) as jumlah,
+                SUM(t.transaksi_total) as total_biaya
+            FROM transaksi t , master_jenis_pengeluaran mjp 
+            WHERE
+                '.$filter.'
+                AND t.id_pengeluaran = mjp.pengeluaran_id
+                AND mjp.pengeluaran_group = "Administrasi"
+            GROUP BY 
+                t.no_rangka,
+                t.kendaraan_stnk,
+                mjp.pengeluaran_jenis,
+                t.transaksi_tanggal
+            ORDER BY t.transaksi_tanggal DESC
+        ')->result();
+    }
 }
