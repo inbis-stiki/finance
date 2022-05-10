e941994912ed1220f3d07de74967cc152d57046f
devisap <ilham.sagitaputra@gmail.com>
2022-05-10 12:44:23 +0700

    fixing bug dashboard management

--- application/controllers/admin/Report.php
+++ application/controllers/admin/Report.php
@@ -115,6 +115,65 @@ class MyPDF extends TCPDF{
         $this->setTableConf();
         $this->Cell(array_sum($w), 6, "Total: Rp.".number_format($total), 1, 0, 'R', 1);
     }
+    public function setTableExp($header,$data,$jenExp) {
+        $this->setTableConf();
+        $this->Ln(10);
+        // Header
+        $w = array(10, 30, 35, 80, 50);
+        $num_headers = count($header);
+        $first = 1;
+        for($i = 0; $i < $num_headers; ++$i) {
+            if($first++ == 1){
+                $this->setCellMargins(25, 0, 0, 0);
+            }else{
+                $this->setCellMargins(0, 0, 0, 0);
+            }
+            
+            $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
+        }
+        $this->Ln();
+        // Color and font restoration
+        $this->SetFillColor(224, 235, 255);
+        $this->SetTextColor(0);
+        $this->SetFont('', '', 10);
+        // Data
+        $fill = 0;
+        $no = 1;
+
+        $total = 0;
+        foreach ($data as $item) {
+            $total += (int)$item->total_biaya;
+            $harga = (int)$item->total_biaya / (int)$item->jumlah;
+
+            // $this->setCellMargins(25, 0, 0, 0);
+            $cellcount = array();
+            //write text first
+            $startX = $this->GetX();
+            $startY = $this->GetY();
+            //draw cells and record maximum cellcount
+            //cell height is 6 and width is 80
+     
+            $cellcount[] = $this->MultiCell($w[0], 6, $no++, 0, 'C',$fill,0);
+            $cellcount[] = $this->MultiCell($w[1], 6, date_format(date_create($item->transaksi_tanggal), 'd/m/Y'), 0, 'C',$fill,0);
+            $cellcount[] = $this->MultiCell($w[2], 6, $item->kendaraan_stnk, 0, 'C',$fill,0);
+            $cellcount[] = $this->MultiCell($w[3], 6, $item->transaksi_keterangan, 0, 'L',$fill,0);
+            $cellcount[] = $this->MultiCell($w[4], 6, number_format($item->total_biaya), 0, 'R',$fill,0);
+            $this->SetXY($startX,$startY);
+            
+            $maxnocells = max($cellcount);
+            $in = 0;
+            for($i = 0; $i < count($w); $i++) {
+                $this->MultiCell($w[$i], $maxnocells * 6,'','LR','L',$fill,0);
+                $in++;
+            }
+
+            $this->Ln();
+            $fill=!$fill;
+        }
+
+        $this->setTableConf();
+        $this->Cell(array_sum($w), 6, "Total: Rp.".number_format($total), 1, 0, 'R', 1);
+    }
 }
 class Report extends CI_Controller{
     public function __construct(){
@@ -126,6 +185,8 @@ class Report extends CI_Controller{
             $this->administrasi($_POST['period'], $_POST['tgl']);
         }else if($_POST['jenis'] == '2'){
             $this->maintenance($_POST['period'], $_POST['tgl']);
+        }else{
+            $this->expense($_POST['period'], $_POST['tgl'], $_POST['jenis']);
         }
     }
     public function administrasi($filter, $tgl){
@@ -216,6 +277,66 @@ class Report extends CI_Controller{
         // $pdf->writeHTML($this->setTablee());
         $pdf->Output('Laporan_Maintenance_Tanggal_'.$this->dateformat->getFullMonth(date('d-m-Y')).'.pdf', 'I');
     }
+    public function expense($filter, $tgl, $jenExp){
+        $jenExpense = "";
+        if($jenExp == '3'){
+            $jenExpense = "BBM";
+        }else if($jenExp == '4'){
+            $jenExpense = "Driver";
+        }else if($jenExp == '5'){
+            $jenExpense = "Lain - Lain";
+        }
+        $pdf = new MyPDF('Landscape', PDF_UNIT, "A4", true, 'UTF-8', false);
+        $pdf->HeaderTitle = 'Expense ('.$jenExpense.')';
+
+        $data = "";
+        if($filter == '1'){
+            $date  = explode(' ', $tgl);
+            $month = date_create($date[0]);
+            $data = $this->getDataMain(['filter' => '1', 'month' => date_format($month, 'n'), 'year' => $date[1], 'jenExp' => $jenExpense]);
+            $pdf->HeaderTitle2 = $tgl;
+        }else if($filter == '2'){
+            $date       = explode(' to ', $tgl);
+            $startDate  = date_create($date[0]);
+            $endDate    = date_create($date[1]);
+            $data = $this->getDataMain(['filter' => '2', 'start' => date_format($startDate, 'Y-m-d'), 'end' => date_format($endDate, 'Y-m-d'), 'jenExp' => $jenExpense]);
+            $pdf->HeaderTitle2 = date_format($startDate, 'd-m-Y')." - ".date_format($endDate, 'd-m-Y');
+        }else if($filter == '3'){
+            $date = date_create($tgl);
+            $data = $this->getDataMain(['filter' => '3', 'date' => date_format($date, 'Y-m-d'), 'jenExp' => $jenExpense]);
+            $pdf->HeaderTitle2 = date_format($date, 'j F Y');
+        }
+
+        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 006', PDF_HEADER_STRING);
+        // set header and footer fonts
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $pdf->SetMargins(10, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        // set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+        // add a page
+        $pdf->AddPage();
+        if($jenExp == '3'){
+            $header = array('No', 'Tanggal', 'Nomor Polisi', 'Keterangan', 'Total Biaya');
+        }else if($jenExp == '4'){
+            $header = array('No', 'Tanggal', 'Nomor Polisi', 'Total Hari Masuk', 'Fee per Hari', 'Total Fee');
+        }else if($jenExp == '5'){
+            $header = array('No', 'Tanggal', 'Nomor Polisi', 'Keterangan', 'Qty', 'Harga', 'Total Harga');
+        }
+        
+        $pdf->setTableExp($header, $data, $jenExp);
+        // $pdf->Ln(10);
+        // $pdf->writeHTML($this->setTablee());
+        $pdf->Output('Laporan_Expense-'.$jenExpense.'_Tanggal_'.$this->dateformat->getFullMonth(date('d-m-Y')).'.pdf', 'I');
+    }
     public function getDataAdm($param){
         $filter = "";
         if($param['filter'] == '1'){ // getByMonth
@@ -289,6 +410,47 @@ class Report extends CI_Controller{
                 AND t.id_pengeluaran = mjp.pengeluaran_id
                 AND mjp.pengeluaran_group = "Maintenance"
                 AND t.id_sparepart = ms.sparepart_id 
+            GROUP BY 
+                t.transaksi_keterangan ,
+                t.id_sparepart ,
+                t.no_rangka ,
+                t.kendaraan_stnk ,
+                t.transaksi_tanggal      
+            ORDER BY t.transaksi_tanggal DESC  
+        ')->result();
+    }
+    public function getDataExp($param){
+        $filter = "";
+        if($param['filter'] == '1'){ // getByMonth
+            $filter = '
+                YEAR(t.transaksi_tanggal) = "'.$param['year'].'"
+                AND MONTH(t.transaksi_tanggal) = "'.$param['month'].'"
+            ';
+        }else if($param['filter'] == "2"){
+            $filter = '
+                t.transaksi_tanggal >= "'.$param['start'].'"
+                AND t.transaksi_tanggal <= "'.$param['end'].'"
+            ';
+        }else if($param['filter'] == "3"){
+            $filter = '
+                t.transaksi_tanggal = "'.$param['date'].'"
+            ';
+        }
+
+        return $this->db->query('
+            SELECT 
+                t.* ,
+                mjp.pengeluaran_group as jenis_pengeluaran,
+                mjp.pengeluaran_jenis as pengeluaran,
+                SUM(t.transaksi_jumlah) as jumlah,
+                SUM(t.transaksi_total) as total_biaya
+            FROM 
+                transaksi t , 
+                master_jenis_pengeluaran mjp
+            WHERE
+                '.$filter.'
+                AND t.id_pengeluaran = mjp.pengeluaran_id
+                AND mjp.pengeluaran_jenis  = "'.$param['jenExp'].'"
             GROUP BY 
                 t.transaksi_keterangan ,
                 t.id_sparepart ,
